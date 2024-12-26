b6bfc621b0de51ca33bb71ffc78aa22f573c1c8f
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2024-12-26 22:14:25 +0530

    Update step02.py

--- step02.py
+++ step02.py
@@ -272,7 +272,7 @@ def upload_to_ftp(ftp_host, ftp_username, ftp_password, filename, content, id):
 if __name__ == "__main__":
     try:  
       # Example query
-      query = "SELECT * FROM app_data WHERE status = 'BUILDING_01' ORDER BY id DESC LIMIT 1"
+      query = "SELECT * FROM app_data WHERE status = 'BUILDING_02' ORDER BY id DESC LIMIT 1"
   
       # Execute the query
       execute_query(host, username, password, database, query)
